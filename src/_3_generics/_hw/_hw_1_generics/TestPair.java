99e4d78db35b70afe37fa44d346dba4fda644960
Dzmitry-K <49922379+Dzmitry-K@users.noreply.github.com>
2019-05-26 13:09:10 +0300

    Add files via upload

--- src/_3_generics/_hw/_hw_1_generics/TestPair.java
+++ src/_3_generics/_hw/_hw_1_generics/TestPair.java
@@ -3,15 +3,17 @@ package _3_generics._hw._hw_1_generics;
 public class TestPair {
     public static void main(String[] args) {
         Pair pair = new Pair<>("qwe", "ewq");
+        Pair pair2 = new Pair<>(3, 4);
         System.out.println(pair.getFirstElem());
         System.out.println(pair.getSecondElem());
         pair.setFirstElem(1);
         pair.setSecondElem(2);
         System.out.println(pair.getFirstElem());
-        System.out.println(pair.getSecondElem());
 
         System.out.println(new MinMaxPair<Integer>(1, 2).maxPair());
         System.out.println(new MinMaxPair<Integer>(1, 2).minPair());
+        System.out.println(new MinMaxPair<>(pair2).minPair());
+        System.out.println(new MinMaxPair<>(pair2).maxPair());
         System.out.println(new MinMaxPair<String>("abc", "bcd").maxPair());
         System.out.println(new MinMaxPair<String>("abc", "bcd").minPair());
 
