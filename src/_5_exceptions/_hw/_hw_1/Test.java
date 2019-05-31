b30ae799b78a4cdf17cd5636942ce006a1fd251c
Dzmitry_K <dzmitry.karachun@mail.ru>
2019-05-31 11:30:19 +0300

    UPDATE class TestGarage

--- src/_5_exceptions/_hw/_hw_1/Test.java
+++ src/_5_exceptions/_hw/_hw_1/Test.java
@@ -3,7 +3,7 @@ package _5_exceptions._hw._hw_1;
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
 
         List<Double> list = new ScannerDoubleFromConsole().readValues();
         System.out.println(list);
