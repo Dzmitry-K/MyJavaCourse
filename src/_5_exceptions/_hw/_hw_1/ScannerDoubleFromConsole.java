402b31c385cd18bd07a5f29a01cc83980661d3bf
Dzmitry Karachun <dzmitry.karachun@mail.ru>
2019-05-31 10:26:41 +0300

    ADD hw_exceptions

---
+++ src/_5_exceptions/_hw/_hw_1/ScannerDoubleFromConsole.java
@@ -0,0 +1,27 @@
+/**
+ *      Напишите метод public List<Double> readValues() throws ..., читающий числа
+ *      с плавающей точкой из ввода консоли, через Scanner. Сгенерируйте подходящие исключения,
+ *      если ввод если введены данные, не относящиеся к числам с плавающей точкой.
+ *      Ввод:  1 2 3 4 aaaa
+ */
+
+package _5_exceptions._hw._hw_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScannerDoubleFromConsole {
+    public List readData() {
+        Scanner in = new Scanner(System.in);
+        List<Double> doubleList = new ArrayList<>();
+        try {
+            while ((in.hasNext())) {
+                doubleList.add(Double.valueOf(in.next()));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        return doubleList;
+    }
+}
---
+++ src/_5_exceptions/_hw/_hw_1/Test.java
@@ -0,0 +1,7 @@
+package _5_exceptions._hw._hw_1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new ScannerDoubleFromConsole().readData());
+    }
+}
